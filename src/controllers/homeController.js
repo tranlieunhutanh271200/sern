da50b6d65b62d7e85c95a43976533e413b8ca192
Anh Tran <18110077@student.hcmute.edu.vn>
2022-08-18 23:11:45 +0700

    login

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -26,9 +26,53 @@ let postCRUD = async (req, res) => {
     await CRUDService.createNewUser(req.body);
     return res.send('post crud from server')
 }
+let displaygetCRUD = async (req, res) => {
+    let data = await CRUDService.getAllUser();
+    console.log(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: data
+    })
+
+}
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        console.log(userData);
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+    }
+    else {
+        return res.send('User not found');
+    }
+}
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
+    })
+}
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+
+    if (id) {
+        await CRUDService.deleteUserById(id);
+        return res.send('Delete user succeed!')
+    }
+    else {
+        return res.send('Delete user fail!')
+    }
+
+}
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
+    displaygetCRUD: displaygetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 }
